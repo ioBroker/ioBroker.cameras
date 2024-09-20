38d726f280a9c9a1bee1f3e2095bc15414f5252b
GermanBluefox <dogafox@gmail.com>
2024-09-20 16:27:06 +0800

    Replace craco with vite

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -3,9 +3,10 @@ import type { UserConfig } from 'vite';
 export default {
     build: {
         outDir: 'build',
+        sourcemap: true,
         rollupOptions: {
             onwarn: (warning: any, warn: (log: any) => void) => {
-                if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+                if (warning.code === 'MODULE_LEVEL_DIRECTIVE' || warning.code === 'SOURCEMAP_ERROR') {
                     return;
                 }
                 warn(warning);
