a50177d8ad6925c38337c0bc2f97563a0b92feb3
GermanBluefox <dogafox@gmail.com>
2024-10-02 01:10:06 +0800

    Formatting

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -1,13 +1,5 @@
 import config from '@iobroker/eslint-config';
 
-// disable temporary the rule 'jsdoc/require-param' and enable 'jsdoc/require-jsdoc'
-config.forEach(rule => {
-    if (rule?.plugins?.jsdoc) {
-        rule.rules['jsdoc/require-jsdoc'] = 'off';
-        rule.rules['jsdoc/require-param'] = 'off';
-    }
-});
-
 export default [
     ...config,
     {
@@ -18,6 +10,13 @@ export default [
                 },
                 tsconfigRootDir: import.meta.dirname,
             },
+        },
+    },
+    {
+        // disable temporary the rule 'jsdoc/require-param' and enable 'jsdoc/require-jsdoc'
+        rules: {
+            'jsdoc/require-jsdoc': 'off',
+            'jsdoc/require-param': 'off',
         },
     },
 ];
