7dafc2489bb88dffc989139650937e02da168d5b
GermanBluefox <dogafox@gmail.com>
2026-08-16 14:42:42 +0300

    Updated packages

--- admin/dm-widgets/customDevices.js
+++ admin/dm-widgets/customDevices.js
@@ -1,1 +1,1 @@
-import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__DevicesWidgetCamerasSet__customDevices_js-C9hpebq2.js";export{t as get,e as init};
+import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__DevicesWidgetCamerasSet__customDevices_js-mbORQ5Xp.js";export{t as get,e as init};
